95eb6755607d2980ae0dc29e744f3d73c143083f
nyquil dabossman <mfaulkner3447@gmail.com>
2025-02-22 15:17:38 +0000

    feat: enter username

--- number_guess.sh
+++ number_guess.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
 PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+
+ASK_USERNAME(){
+  echo -e "\nEnter your username:"
+  read USERNAME
+
+  USERNAME_CHARACTERS=$(echo $USERNAME | wc -c)
+  if [[ $USERNAME_CHARACTERS -gt 22 ]]
+  then
+    ASK_USERNAME
+  fi
+}
